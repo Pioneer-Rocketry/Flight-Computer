f4ab2993b3c6214fef4a77839138d6b893649f90
CSutter5 <contact@csutter.dev>
2025-07-03 02:48:26 +0000

    defined registers, and created basic begin

--- Core/Inc/SPI_Devices/radio_RFM95W.h
+++ Core/Inc/SPI_Devices/radio_RFM95W.h
@@ -7,16 +7,61 @@
 #include "data.h"
 
 // Registers
+#define RFM95W_FIFO					0x00
+#define RFM95W_OP_MODE				0x01
+#define RFM95W_FRF_MSB				0x06
+#define RFM95W_FRF_MID				0x07
+#define RFM95W_FRF_LSB				0x08
+#define RFM95W_PA_CONFIG			0x09
+#define RFM95W_PA_RAMP				0x0A
+#define RFM95W_OCP					0x0B
+#define RFM95W_LNA					0x0C
+#define RFM95W_FIFO_ADDR_PTR		0x0D
+#define RFM95W_FIFO_TX_BASE_ADDR	0x0E
+#define RFM95W_FIFO_RX_BASE_ADDR	0x0F
+#define RFM95W_IRQ_FLAGS	 		0x10
+#define RFM95W_IRQ_MASK 			0x11
+#define RFM95W_FREQ_IF_MSB 			0x12
+#define RFM95W_FREQ_IF_LSB 			0x13
+#define RFM95W_SYMB_TIMEOUT_MSB 	0x14
+#define RFM95W_SYMB_TIMEOUT_LSB 	0x15
+#define RFM95W_TX_CFG 				0x16
+#define RFM95W_PAYLOAD_LENGTH 		0x17
+#define RFM95W_PREAMBLE_MSB 		0x18
+#define RFM95W_PREAMBLE_LSB 		0x19
+#define RFM95W_MODULATION_CFG 		0x1A
+#define RFM95W_RF_MODE 				0x1B
+#define RFM95W_HOP_PERIOD 			0x1C
+#define RFM95W_NB_RX_BYTES 			0x1D
+#define RFM95W_RX_HEADER_INFO 		0x1E
+#define RFM95W_RX_HEADED_CNT_VALUE	0x1F
+#define RFM95W_RX_PACKET_CNT_VALUE	0x20
+#define RFM95W_MODEM_STAT 			0x21
+#define RFM95W_PKT_SNR_VALUE 		0x22
+#define RFM95W_RSSI_VALUE 			0x23
+#define RFM95W_PKT_RSSI_VALUE 		0x24
+#define RFM95W_HOP_CHANNEL 			0x25
+#define RFM95W_RX_DATA_ADDR 		0x26
+#define RFM95W_DIO_MAPPING_1 		0x40
+#define RFM95W_DIO_MAPPING_2 		0x41
+#define RFM95W_VERSION 				0x42
+#define RFM95W_TCXO 				0x43
+#define RFM95W_PA_DAC 				0x4B
+#define RFM95W_FORMER_TEMP 			0x58
+#define RFM95W_AGC_REG 				0x61
+#define RFM95W_AGC_THRESH_1 		0x62
+#define RFM95W_AGC_THRESH_2 		0x63
+#define RFM95W_AGC_THRESH_3			0x64
+
 
 class Radio_RFM95W : public SPI_Device
 {
 private:
 
-    void writeRegister(uint16_t reg, uint8_t value);
-    uint8_t readRegister(uint16_t reg);
+	float frequency = 915.0;
 
 public:
-    Radio_RFM95W(Data *data, SPI_HandleTypeDef *spi, GPIO_TypeDef *port, uint16_t pin);
+    Radio_RFM95W(Data *data, SPI_HandleTypeDef *spi, GPIO_TypeDef *port, uint16_t pin, float frequency);
 
     bool begin() override;
     void loop() override;
--- Core/Src/SPI_Devices/radio_RFM95W.cpp
+++ Core/Src/SPI_Devices/radio_RFM95W.cpp
@@ -1,10 +1,60 @@
 #include "SPI_Devices/radio_RFM95W.h"
 
-Radio_RFM95W::Radio_RFM95W(Data *data, SPI_HandleTypeDef *spi, GPIO_TypeDef *port, uint16_t pin)
+Radio_RFM95W::Radio_RFM95W(Data *data, SPI_HandleTypeDef *spi, GPIO_TypeDef *port, uint16_t pin, float frequency)
     : SPI_Device(data, spi, port, pin, "RFM95W") {
+
+	this->frequency = frequency;
 }
 
 bool Radio_RFM95W::begin() {
+
+    /* Radio Configuration */
+    uint8_t config = 0;
+
+	// OP_MODE page 87
+	config  = 0;
+	config |= 0b1   << 7; // Enable LoRa Mode
+	config |= 0b0   << 6; // Access to LoRa register page
+	config |= 0b00  << 4; // Reserved
+	config |= 0b0   << 3; // High Frequency Mode
+	config |= 0b000 << 0; // Sleep Mode
+	write_SPI(RFM95W_OP_MODE, &config);
+
+	// FR_MSB
+
+	// FR_MID
+
+	// FR_LSB
+
+	// PA_CONFIG
+	config  = 0;
+	config |= 0b1    << 7; // Enabled PA_BOOST
+	config |= 0b111  << 4; // Set max power
+	config |= 0b1111 << 0; // Set output power
+	write_SPI(RFM95W_PA_CONFIG, &config);
+
+	// PA_RAMP
+	config  = 0;
+	config |= 0b0000 << 3; // Reserved
+	config |= 0b1001 << 0; // PA Ramp Time
+
+	write_SPI(RFM95W_PA_RAMP, &config);
+
+	// OCP
+	config  = 0;
+	config |= 0b00 	  << 6; // Unused
+	config |= 0b0 	  << 5; // Turn off OCP
+	config |= 0b11111 << 0; // OCP Trim
+	write_SPI(RFM95W_OCP, &config);
+
+	// LNA
+	config  = 0;
+	config |= 0b001	<< 5; // Set LNA Gain to Max
+	config |= 0b00 	<< 3; // Default LNA current
+	config |= 0b00 	<< 1; // Reserved
+	config |= 0b11 	<< 0; // Enable boost
+	write_SPI(RFM95W_LNA, &config);
+
     return true;
 }
 
